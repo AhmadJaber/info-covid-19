b054bc1ea5de5643df27e34de328282ce07bae11
AhmadJaber <jaber131706@gmail.com>
2020-07-12 17:18:19 +0600

    datatable component  functionality added

--- src/components/DataTable/DataTable.component.jsx
+++ src/components/DataTable/DataTable.component.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { withStyles, makeStyles } from '@material-ui/core/styles';
+import { withStyles } from '@material-ui/core/styles';
 import {
   Table,
   TableBody,
@@ -9,35 +9,32 @@ import {
   TableRow,
   Paper,
 } from '@material-ui/core';
-
 import { fetchCountryInfo } from '../../api';
 
-const StyledTableCell = withStyles((theme) => ({
+import styles from './DataTable.module.css';
+
+const StyledTableCell = withStyles(() => ({
   head: {
-    backgroundColor: theme.palette.common.black,
-    color: theme.palette.common.white,
+    backgroundColor: '#fff',
+    color: '#212529',
+    fontSize: 14,
+    textAlign: 'center',
   },
   body: {
     fontSize: 14,
+    textAlign: 'center',
   },
 }))(TableCell);
 
 const StyledTableRow = withStyles((theme) => ({
   root: {
-    '&:nth-of-type(odd)': {
+    '&:first-child': {
       backgroundColor: theme.palette.action.hover,
     },
   },
 }))(TableRow);
 
-const useStyles = makeStyles({
-  table: {
-    minWidth: 700,
-  },
-});
-
 const DataTable = () => {
-  const classes = useStyles();
   const [countryDataRows, setCountryDataRows] = useState([]);
 
   useEffect(() => {
@@ -50,36 +47,85 @@ const DataTable = () => {
   }, []);
 
   return (
-    <TableContainer component={Paper}>
-      <Table className={classes.table} aria-label='customized table'>
-        <TableHead>
-          <TableRow>
-            <StyledTableCell>#</StyledTableCell>
-            <StyledTableCell>Country</StyledTableCell>
-            <StyledTableCell>Cases</StyledTableCell>
-            <StyledTableCell>Deaths</StyledTableCell>
-            <StyledTableCell>Critical</StyledTableCell>
-            <StyledTableCell>Recovered</StyledTableCell>
-            <StyledTableCell>Today's Cases</StyledTableCell>
-            <StyledTableCell>Today's Deaths</StyledTableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {countryDataRows.map((row, index) => (
-            <StyledTableRow key={row.country}>
-              <StyledTableCell component='th' scope='row'>
-                {index + 1}
+    <Paper variant='outlined'>
+      <TableContainer>
+        <Table className={styles.table} aria-label='customized table'>
+          <TableHead>
+            <TableRow>
+              <StyledTableCell className={styles.tableCells}>#</StyledTableCell>
+              <StyledTableCell className={styles.tableCells}>
+                Country
               </StyledTableCell>
-              <StyledTableCell align='right'>{row.country}</StyledTableCell>
-              <StyledTableCell align='right'>{row.cases}</StyledTableCell>
-              <StyledTableCell align='right'>{row.deaths}</StyledTableCell>
-              <StyledTableCell align='right'>{row.critical}</StyledTableCell>
-              <StyledTableCell align='right'>{row.recovered}</StyledTableCell>
-            </StyledTableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+              <StyledTableCell className={styles.tableCells}>
+                Cases
+              </StyledTableCell>
+              <StyledTableCell className={styles.tableCells}>
+                Deaths
+              </StyledTableCell>
+              <StyledTableCell className={styles.tableCells}>
+                Critical
+              </StyledTableCell>
+              <StyledTableCell className={styles.tableCells}>
+                Recovered
+              </StyledTableCell>
+              <StyledTableCell className={styles.tableCells}>
+                Today&apos;s Cases
+              </StyledTableCell>
+              <StyledTableCell className={styles.tableCells}>
+                Today&apos;s Deaths
+              </StyledTableCell>
+              <StyledTableCell className={styles.tableCells}>
+                Cases Per 1M
+              </StyledTableCell>
+              <StyledTableCell className={styles.tableCells}>
+                Deaths Per 1M
+              </StyledTableCell>
+            </TableRow>
+          </TableHead>
+
+          <TableBody>
+            {countryDataRows.map((row, index) => (
+              <StyledTableRow key={row.country}>
+                <StyledTableCell
+                  className={styles.tableCells}
+                  component='th'
+                  scope='row'
+                >
+                  {index + 1}
+                </StyledTableCell>
+                <StyledTableCell className={styles.tableCells}>
+                  {row.country}
+                </StyledTableCell>
+                <StyledTableCell className={styles.tableCells}>
+                  {row.cases}
+                </StyledTableCell>
+                <StyledTableCell className={styles.tableCells}>
+                  {row.deaths}
+                </StyledTableCell>
+                <StyledTableCell className={styles.tableCells}>
+                  {row.critical}
+                </StyledTableCell>
+                <StyledTableCell className={styles.tableCells}>
+                  {row.recovered}
+                </StyledTableCell>
+                <StyledTableCell className={styles.tableCells}>
+                  {row.todayCases}
+                </StyledTableCell>
+                <StyledTableCell className={styles.tableCells}>
+                  {row.todayDeaths}
+                </StyledTableCell>
+                <StyledTableCell className={styles.tableCells}>
+                  {row.casesPerOneMillion}
+                </StyledTableCell>
+                <StyledTableCell className={styles.tableCells}>
+                  {row.deathsPerOneMillion}
+                </StyledTableCell>
+              </StyledTableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Paper>
   );
 };
 

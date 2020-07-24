d5be2257d59a8242c6cd4fd0ec4562733d967853
AhmadJaber <jaber131706@gmail.com>
2020-07-24 22:15:02 +0600

    header added, setup svg for parcel

--- src/components/Header/Header.component.jsx
+++ src/components/Header/Header.component.jsx
@@ -2,16 +2,41 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { Typography } from '@material-ui/core';
 
-import { ReactComponents as Logo } from '../../assets/coronavirus.svg';
+import Logo from '../../assets/coronavirus.svg';
+
+const useStyles = makeStyles({
+  headerContainer: {
+    padding: '1em',
+  },
+  headerMain: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+  logo: {
+    height: '2.25em',
+    width: '2.25em',
+    marginRight: '.6em',
+  },
+  title: {
+    fontSize: '2rem',
+    fontWeight: 600,
+  },
+});
 
 const Header = () => {
+  const classes = useStyles();
+
   return (
-    <div className='headerContainer'>
+    <div className={classes.headerContainer}>
       <div>
-        <Logo />
-        <Typography component='h1' variant='h2'>
-          INFO COVID-19
-        </Typography>
+        <div className={classes.headerMain}>
+          <div>
+            <Logo className={classes.logo} />
+          </div>
+          <Typography component='h1' className={classes.title}>
+            INFO COVID-19
+          </Typography>
+        </div>
       </div>
     </div>
   );
